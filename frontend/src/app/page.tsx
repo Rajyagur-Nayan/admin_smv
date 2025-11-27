71a81761da16ab7cbb3f9865d92e83785a02668c
Rajyaguru Nayan <nayanrajyaguru59@gamil.com>
2025-11-27 13:41:30 +0530

    first commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SchoolDashboardPage from "./admin-dashboard/page";
+
+const page = () => {
+  return (
+    <div>
+      {/* <HomePage /> */}
+      <SchoolDashboardPage />
+    </div>
+  );
+};
+
+export default page;
